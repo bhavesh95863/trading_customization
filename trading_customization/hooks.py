79c7d93ba76de9ca8a636f79959fb6d76a1570e2
root <root@frappe.asia-southeast1-b.c.venia-262808.internal>
2020-10-11 22:48:43 +0000

    fix: custom script not load from hook

--- trading_customization/hooks.py
+++ trading_customization/hooks.py
@@ -26,7 +26,12 @@ app_license = "MIT"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_js = {
+	"Sales Order" : "public/js/sales_order.js",
+	"Sales Invoice" : "public/js/sales_invoice.js",
+	"Purchase Order" : "public/js/purchase_order.js",
+	"Purchase Invoice" : "public/js/purchase_invoice.js"
+}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
---
+++ trading_customization/public/js/purchase_order.js
@@ -0,0 +1,37 @@
+frappe.ui.form.on('Purchase Order', {
+	refresh(frm) {
+		// your code here
+	}
+})
+
+frappe.ui.form.on("Purchase Order", {
+	onload: function(frm) {
+		let items = frm.get_field("items").grid;
+		items.toggle_enable("amount", true);
+	}
+});
+frappe.ui.form.on("Purchase Order Item", {
+	amount: function(frm,cdt,cdn) {
+	    var doc = locals[cdt][cdn];
+	    if (doc.qty && doc.amount){
+	         frappe.model.set_value(cdt,cdn,"rate",doc.amount/doc.qty)
+	    }
+	   
+	},
+	item_code(frm,cdt,cdn) {
+		// your code here
+		var doc = locals[cdt][cdn];
+		if (doc.item_code){
+		    frappe.call({
+		        method:"trading_customization.api.get_last_transaction",
+		        args:{'c_doctype':doc.doctype,'p_doctype':frm.doc.doctype,'item':doc.item_code,'parent':doc.parent},
+		        callback:function(r){
+		            console.log(r)
+		            if (r.message){
+		                $(frm.fields_dict.last_transaction_custom.wrapper).empty().html(r.message)
+		            }
+		        }
+		    })
+		}
+	}
+});
